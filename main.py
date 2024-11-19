aa1444c6b8caabf5e20d7ef6e1e9db3ee1a1c269
Speedymr01 <MR281109@protonmail.com>
2024-11-19 14:59:05 +0000

    initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from settings import *
+
+if __name__ == '__main__':
+    #if AGREE:
+        #game = Game()
+        #game.run()
+    #else:
+        print("You have not agreed to the t's and c's, find them in the settings file")
